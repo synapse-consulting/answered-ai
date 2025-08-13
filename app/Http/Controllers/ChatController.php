2ec1ecf7254348e57c737c379d3c09d0961e9667
Osama Ashfaq <osama.ashfaq19@outlook.com>
2025-08-13 13:49:53 +0500

    updated timestamp to created at in chats

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -27,7 +27,7 @@ class ChatController extends Controller
     public function getMessages(Request $request, $contactId)
     {
         $chats = Chat::where('contact_id', $contactId)
-            ->orderBy('timestamp', 'asc')
+            ->latest()
             ->get();
 
         $contact = CompanyContact::with('contact')->where('contact_id', $contactId)->first(); 
