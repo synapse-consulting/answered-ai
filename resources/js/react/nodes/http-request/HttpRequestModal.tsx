5dee0fcef9a94fdff33885d966e4cb8570f2562b
Ali Kashan <alikashan@synapse-consulting.com.pk>
2025-08-26 10:12:49 +0500

    removed rfjs fake implementation

--- resources/js/react/nodes/http-request/HttpRequestModal.tsx
+++ resources/js/react/nodes/http-request/HttpRequestModal.tsx
@@ -148,7 +148,7 @@ export const HttpRequestModal: React.FC<HttpRequestModalProps> = ({
       description="Configure your HTTP request parameters"
       maxWidth="2xl"
       footer={
-        <div className="flex justify-end gap-3 pt-4 border-t border-gray-200 dark:border-gray-700">
+        <div className="flex justify-end gap-3 ">
           <Button variant="secondary" onClick={onClose}>
             Close
           </Button>
